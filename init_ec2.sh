526a41ed6a8a8219cd27679809a9091c939fd44e
Venkat Arun <venkatar@mit.edu>
2019-10-19 16:59:30 -0400

    In init_ec2, don't set ccp as default congestion control

--- init_ec2.sh
+++ init_ec2.sh
@@ -1,5 +1,4 @@
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv4.tcp_congestion_control=ccp
 cd $HOME/ccp-kernel
 sudo ./ccp_kernel_load ipc=0
 export PYTHONPATH=$PYTHONPATH:$HOME/6.829-pset2-3
